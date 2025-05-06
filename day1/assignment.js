38193e4e95731135068d221deb475b7f0d060c4e
Benjamin <ajanakub30@gmail.com>
2025-05-06 16:04:26 +0100

    solution

--- day1/assignment.js
+++ day1/assignment.js
@@ -1,44 +1,66 @@
-      //Exercise 1: Write a function that takes two numbers as arguments and returns true if both numbers are greater than 10, otherwise returns false.
-      //Exercise 2: Write a function that takes two numbers as arguments and returns true if at least one number is greater than 10, otherwise returns false.
-      //Exercise 3: Write a function that takes a number as an argument and returns true if the number is even, otherwise returns false.
-      //Exercise on local and global variables: Write a function that takes a number as an argument and returns true if the number is greater than 10, otherwise returns false. Inside the function, declare a local variable with the same name as a global variable and log both variables to the console.
+//Exercise 1: Write a function that takes two numbers as arguments and returns true if both numbers are greater than 10, otherwise returns false.
+function checkIfBothNumsIsGreaterThanTen(num1, num2) {
+  return num1 > 10 && num2 > 10;
+}
+//Exercise 2: Write a function that takes two numbers as arguments and returns true if at least one number is greater than 10, otherwise returns false.
+function checkIfAtLeastOneNumIsGreaterThanTen(num1, num2) {
+  return num1 > 10 || num2 > 10;
+}
+//Exercise 3: Write a function that takes a number as an argument and returns true if the number is even, otherwise returns false.
+function checkIfEvenOrOdd(num) {
+  return num % 2 === 0;
+}
 
+//Exercise on local and global variables: Write a function that takes a number as an argument and returns true if the number is greater than 10, otherwise returns false. Inside the function, declare a local variable with the same name as a global variable and log both variables to the console.
+let num = 10;
 
-      //Exercise 5:
-      let x = 10;
+function checkIfNumIsGreaterThanTen(number) {
+  let num = number;
+  console.log(num);
 
-      function testScope() {
-        let x = 20;
-        console.log("Inside function: ", x);
-      }
-      testScope();
-      console.log("Outside function: ", x);
+  return num > 10;
+}
+checkIfNumIsGreaterThanTen(14);
+console.log(num);
 
-      //Task: Explain why the outputs are different. What happens if you remove "let x =20;" inside the function?
+//Exercise 5:
+let x = 10;
 
-      //Exercise 6: Even or Odd
-      let number = 7;
+function testScope() {
+  let x = 20;
+  console.log("Inside function: ", x);
+}
+testScope();
+console.log("Outside function: ", x);
 
-      //Use a ternary operator to log "Even" or "Odd"
+//Task: Explain why the outputs are different. What happens if you remove "let x =20;" inside the function?
 
-      let result = // your code here
+//Exercise 6: Even or Odd
+let number = 7;
 
-      console.log(result);
+//Use a ternary operator to log "Even" or "Odd"
 
-      //Task: Write a ternary expression that checks if number is even or odd
+let result = number % 2 == 0 ? "Even" : "Odd";
 
-      //Exercise 7: Replace if-else with Ternary
+console.log(result);
 
-      let isLoggedIn = true;
+//Task: Write a ternary expression that checks if number is even or odd
 
-      let welcomeMessage;
+//Exercise 7: Replace if-else with Ternary
 
-      if (isLoggedIn) {
-        welcomeMessage = "Welcome back!";
-      }else{
-        welcomeMessage = "Please log in.";
-      }     
+let isLoggedIn = true;
 
-      //Rewrite the above logic using a ternary operator
+let welcomeMessage;
 
-      //Task: Convert the if-else block to a sngle line using a ternary operator
+// if (isLoggedIn) {
+//   welcomeMessage = "Welcome back!";
+// }else{
+//   welcomeMessage = "Please log in.";
+// }
+
+welcomeMessage = isLoggedIn ? "Welcome back!" : "Please log in";
+console.log(welcomeMessage);
+
+//Rewrite the above logic using a ternary operator
+
+//Task: Convert the if-else block to a sngle line using a ternary operator
