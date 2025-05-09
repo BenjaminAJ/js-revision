de230fcbcce92359759c566f50f1bcd890fa728a
Benjamin <ajanakub30@gmail.com>
2025-05-09 21:04:16 +0100

    day 4

---
+++ day4/assignment.js
@@ -0,0 +1,4 @@
+//Assignment 1: Difference between innerText and innerHTML
+//Assignment 2: Give 5 examples of HTML Events
+//Assignment 3: Difference between oninput, onkeydown and onkeyup
+//Assignment 4: Difference between onload and DOMContentLoaded   
